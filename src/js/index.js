50b2c9590644c5834647af113e534be6f13d47c4
Maxi Merrillees <maxi.merrillees@gmail.com>
2018-06-22 11:40:18 +1000

    Add first Component as a template

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import '../css/styles.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Component</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
